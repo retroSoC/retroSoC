a32e4bac6a1bdfa76f906aa60389b645ccdb8748
maksyuki <maksyuki@126.com>
2025-01-22 11:09:59 +0800

    style: format the name of signals

--- rtl/retrosoc.v
+++ rtl/retrosoc.v
@@ -23,126 +23,105 @@
 
 `define PICOSOC_MEM ice40up5k_spram
 
-module retrosoc (
-    input clk,
-    output ser_tx,
-    input ser_rx,
-    output led1,
-    output led2,
-    output led3,
-    output led4,
-    output led5,
-    output ledr_n,
-    output ledg_n,
-    output flash_csb,
-    output flash_clk,
+module retrosoc #(
+    parameter integer MEM_WORDS = 32768
+) (
+    input  clk_i,
+    input  rst_n_i,
+    output uart_tx_o,
+    input  uart_rx_i,
+    output led1_o,
+    output led2_o,
+    output led3_o,
+    output led4_o,
+    output led5_o,
+    output led6_o,
+    output led7_o,
+    output flash_csb_o,
+    output flash_clk_o,
     inout  flash_io0,
     inout  flash_io1,
     inout  flash_io2,
     inout  flash_io3
 );
-    parameter integer MEM_WORDS = 32768;
 
-    reg [5:0] reset_cnt = 0;
-    wire resetn = &reset_cnt;
+  wire flash_io0_oe, flash_io0_do, flash_io0_di;
+  wire flash_io1_oe, flash_io1_do, flash_io1_di;
+  wire flash_io2_oe, flash_io2_do, flash_io2_di;
+  wire flash_io3_oe, flash_io3_do, flash_io3_di;
+  wire        iomem_valid;
+  reg         iomem_ready;
+  wire [ 3:0] iomem_wstrb;
+  wire [31:0] iomem_addr;
+  wire [31:0] iomem_wdata;
+  reg  [31:0] iomem_rdata;
+  wire [ 7:0] leds;
+  reg  [31:0] gpio;
 
-    always @(posedge clk) begin
-        reset_cnt <= reset_cnt + !resetn;
+  assign flash_io0    = flash_io0_oe ? flash_io0_do : 1'bz;
+  assign flash_io0_di = flash_io0;
+  assign flash_io1    = flash_io1_oe ? flash_io1_do : 1'bz;
+  assign flash_io1_di = flash_io1;
+  assign flash_io2    = flash_io2_oe ? flash_io2_do : 1'bz;
+  assign flash_io2_di = flash_io2;
+  assign flash_io3    = flash_io3_oe ? flash_io3_do : 1'bz;
+  assign flash_io3_di = flash_io3;
+
+  assign led1_o       = leds[1];
+  assign led2_o       = leds[2];
+  assign led3_o       = leds[3];
+  assign led4_o       = leds[4];
+  assign led5_o       = leds[5];
+  assign led6_o       = leds[6];
+  assign led7_o       = leds[7];
+  assign leds         = gpio;
+
+  always @(posedge clk_i) begin
+    if (!rst_n_i) begin
+      gpio <= 0;
+    end else begin
+      iomem_ready <= 0;
+      if (iomem_valid && !iomem_ready && iomem_addr[31:24] == 8'h03) begin
+        iomem_ready <= 1;
+        iomem_rdata <= gpio;
+        if (iomem_wstrb[0]) gpio[7:0] <= iomem_wdata[7:0];
+        if (iomem_wstrb[1]) gpio[15:8] <= iomem_wdata[15:8];
+        if (iomem_wstrb[2]) gpio[23:16] <= iomem_wdata[23:16];
+        if (iomem_wstrb[3]) gpio[31:24] <= iomem_wdata[31:24];
+      end
     end
+  end
 
-    wire [7:0] leds;
-
-    assign led1 = leds[1];
-    assign led2 = leds[2];
-    assign led3 = leds[3];
-    assign led4 = leds[4];
-    assign led5 = leds[5];
-
-    assign ledr_n = !leds[6];
-    assign ledg_n = !leds[7];
-
-    wire flash_io0_oe, flash_io0_do, flash_io0_di;
-    wire flash_io1_oe, flash_io1_do, flash_io1_di;
-    wire flash_io2_oe, flash_io2_do, flash_io2_di;
-    wire flash_io3_oe, flash_io3_do, flash_io3_di;
-
-    assign flash_io0 = flash_io0_oe ? flash_io0_do : 1'bz;
-    assign flash_io0_di = flash_io0;
-    assign flash_io1 = flash_io1_oe ? flash_io1_do : 1'bz;
-    assign flash_io1_di = flash_io1;
-    assign flash_io2 = flash_io2_oe ? flash_io2_do : 1'bz;
-    assign flash_io2_di = flash_io2;
-    assign flash_io3 = flash_io3_oe ? flash_io3_do : 1'bz;
-    assign flash_io3_di = flash_io3;
-    // SB_IO #(
-    //     .PIN_TYPE(6'b 1010_01),
-    //     .PULLUP(1'b 0)
-    // ) flash_io_buf [3:0] (
-    //     .PACKAGE_PIN({flash_io3, flash_io2, flash_io1, flash_io0}),
-    //     .OUTPUT_ENABLE({flash_io3_oe, flash_io2_oe, flash_io1_oe, flash_io0_oe}),
-    //     .D_OUT_0({flash_io3_do, flash_io2_do, flash_io1_do, flash_io0_do}),
-    //     .D_IN_0({flash_io3_di, flash_io2_di, flash_io1_di, flash_io0_di})
-    // );
-
-    wire        iomem_valid;
-    reg         iomem_ready;
-    wire [3:0]  iomem_wstrb;
-    wire [31:0] iomem_addr;
-    wire [31:0] iomem_wdata;
-    reg  [31:0] iomem_rdata;
-
-    reg [31:0] gpio;
-    assign leds = gpio;
-
-    always @(posedge clk) begin
-        if (!resetn) begin
-            gpio <= 0;
-        end else begin
-            iomem_ready <= 0;
-            if (iomem_valid && !iomem_ready && iomem_addr[31:24] == 8'h 03) begin
-                iomem_ready <= 1;
-                iomem_rdata <= gpio;
-                if (iomem_wstrb[0]) gpio[ 7: 0] <= iomem_wdata[ 7: 0];
-                if (iomem_wstrb[1]) gpio[15: 8] <= iomem_wdata[15: 8];
-                if (iomem_wstrb[2]) gpio[23:16] <= iomem_wdata[23:16];
-                if (iomem_wstrb[3]) gpio[31:24] <= iomem_wdata[31:24];
-            end
-        end
-    end
-
-    picosoc #(
-        .BARREL_SHIFTER(0),
-        .ENABLE_MUL(0),
-        .ENABLE_DIV(1),
-        .ENABLE_FAST_MUL(1),
-        .MEM_WORDS(MEM_WORDS)
-    ) soc (
-        .clk          (clk         ),
-        .resetn       (resetn      ),
-        .ser_tx       (ser_tx      ),
-        .ser_rx       (ser_rx      ),
-        .flash_csb    (flash_csb   ),
-        .flash_clk    (flash_clk   ),
-        .flash_io0_oe (flash_io0_oe),
-        .flash_io1_oe (flash_io1_oe),
-        .flash_io2_oe (flash_io2_oe),
-        .flash_io3_oe (flash_io3_oe),
-        .flash_io0_do (flash_io0_do),
-        .flash_io1_do (flash_io1_do),
-        .flash_io2_do (flash_io2_do),
-        .flash_io3_do (flash_io3_do),
-        .flash_io0_di (flash_io0_di),
-        .flash_io1_di (flash_io1_di),
-        .flash_io2_di (flash_io2_di),
-        .flash_io3_di (flash_io3_di),
-        .irq_5        (1'b0        ),
-        .irq_6        (1'b0        ),
-        .irq_7        (1'b0        ),
-        .iomem_valid  (iomem_valid ),
-        .iomem_ready  (iomem_ready ),
-        .iomem_wstrb  (iomem_wstrb ),
-        .iomem_addr   (iomem_addr  ),
-        .iomem_wdata  (iomem_wdata ),
-        .iomem_rdata  (iomem_rdata )
-    );
+  picosoc #(
+      .MEM_WORDS      (MEM_WORDS),
+      .BARREL_SHIFTER (1),
+      .COMPRESSED_ISA (1),
+      .ENABLE_FAST_MUL(1),
+      .ENABLE_DIV     (1)
+  ) u_picosoc (
+      .clk_i        (clk_i),
+      .rst_n_i      (rst_n_i),
+      .uart_tx_o    (uart_tx_o),
+      .uart_rx_i    (uart_rx_i),
+      .flash_csb_o  (flash_csb_o),
+      .flash_clk_o  (flash_clk_o),
+      .flash_io0_oe (flash_io0_oe),
+      .flash_io1_oe (flash_io1_oe),
+      .flash_io2_oe (flash_io2_oe),
+      .flash_io3_oe (flash_io3_oe),
+      .flash_io0_do (flash_io0_do),
+      .flash_io1_do (flash_io1_do),
+      .flash_io2_do (flash_io2_do),
+      .flash_io3_do (flash_io3_do),
+      .flash_io0_di (flash_io0_di),
+      .flash_io1_di (flash_io1_di),
+      .flash_io2_di (flash_io2_di),
+      .flash_io3_di (flash_io3_di),
+      .iomem_valid_o(iomem_valid),
+      .iomem_ready_i(iomem_ready),
+      .iomem_wstrb_o(iomem_wstrb),
+      .iomem_addr_o (iomem_addr),
+      .iomem_wdata_o(iomem_wdata),
+      .iomem_rdata_i(iomem_rdata)
+  );
 endmodule
--- rtl/retrosoc_tb.v
+++ rtl/retrosoc_tb.v
@@ -20,44 +20,32 @@
 `timescale 1 ns / 1 ps
 
 module retrosoc_tb;
-  reg clk;
-  always #5 clk = (clk === 1'b0);  // 100M
-
   localparam ser_half_period = 53;
-  event ser_sample;
+  event       ser_sample;
 
-  initial begin
-    $dumpfile("retrosoc_tb.vcd");
-    $dumpvars(0, retrosoc_tb);
+  reg         r_clk;
+  reg   [5:0] r_rst_cnt = 0;
+  wire  [6:0] s_leds;
+  wire s_led1, s_led2, s_led3, s_led4, s_led5, s_led6, s_led7;
+  wire s_uart_rx, s_uart_tx;
+  wire s_flash_csb;
+  wire s_flash_clk;
+  wire s_flash_io0;
+  wire s_flash_io1;
+  wire s_flash_io2;
+  wire s_flash_io3;
 
-    repeat (1000) begin
-      repeat (50000) @(posedge clk);
-      // $display("+50000 cycles");
-    end
-    // $finish;
+  always #5 r_clk = (r_clk === 1'b0);  // 100M
+  // always #10 r_clk = (r_clk === 1'b0);  // 50M
+  wire s_rst_n = &r_rst_cnt;
+
+  always @(posedge r_clk) begin
+    r_rst_cnt <= r_rst_cnt + !s_rst_n;
   end
 
-  integer cycle_cnt = 0;
-  always @(posedge clk) begin
-    cycle_cnt <= cycle_cnt + 1;
-  end
-
-  wire led1, led2, led3, led4, led5;
-  wire ledr_n, ledg_n;
-
-  wire [6:0] leds = {!ledg_n, !ledr_n, led5, led4, led3, led2, led1};
-
-  wire       ser_rx;
-  wire       ser_tx;
-  wire       flash_csb;
-  wire       flash_clk;
-  wire       flash_io0;
-  wire       flash_io1;
-  wire       flash_io2;
-  wire       flash_io3;
-
-  always @(leds) begin
-    #1 $display("leds: %b", leds);
+  assign s_leds = {s_led7, s_led6, s_led5, s_led4, s_led3, s_led2, s_led1};
+  always @(s_leds) begin
+    #1 $display("s_leds: %b", s_leds);
   end
 
   retrosoc #(
@@ -66,49 +54,50 @@ module retrosoc_tb;
       // required for intialization of RAM
       .MEM_WORDS(256)
   ) u_retrosoc (
-      .clk      (clk),
-      .led1     (led1),
-      .led2     (led2),
-      .led3     (led3),
-      .led4     (led4),
-      .led5     (led5),
-      .ledr_n   (ledr_n),
-      .ledg_n   (ledg_n),
-      .ser_rx   (ser_rx),
-      .ser_tx   (ser_tx),
-      .flash_csb(flash_csb),
-      .flash_clk(flash_clk),
-      .flash_io0(flash_io0),
-      .flash_io1(flash_io1),
-      .flash_io2(flash_io2),
-      .flash_io3(flash_io3)
+      .clk_i      (r_clk),
+      .rst_n_i    (s_rst_n),
+      .led1_o     (s_led1),
+      .led2_o     (s_led2),
+      .led3_o     (s_led3),
+      .led4_o     (s_led4),
+      .led5_o     (s_led5),
+      .led6_o     (s_led6),
+      .led7_o     (s_led7),
+      .uart_rx_i  (s_uart_rx),
+      .uart_tx_o  (s_uart_tx),
+      .flash_csb_o(s_flash_csb),
+      .flash_clk_o(s_flash_clk),
+      .flash_io0  (s_flash_io0),
+      .flash_io1  (s_flash_io1),
+      .flash_io2  (s_flash_io2),
+      .flash_io3  (s_flash_io3)
   );
 
   spiflash u_spiflash (
-      .csb(flash_csb),
-      .clk(flash_clk),
-      .io0(flash_io0),
-      .io1(flash_io1),
-      .io2(flash_io2),
-      .io3(flash_io3)
+      .csb(s_flash_csb),
+      .clk(s_flash_clk),
+      .io0(s_flash_io0),
+      .io1(s_flash_io1),
+      .io2(s_flash_io2),
+      .io3(s_flash_io3)
   );
 
   reg [7:0] buffer;
   always begin
-    @(negedge ser_tx);
+    @(negedge s_uart_tx);
 
-    repeat (ser_half_period) @(posedge clk);
+    repeat (ser_half_period) @(posedge r_clk);
     ->ser_sample;  // start bit
 
     repeat (8) begin
-      repeat (ser_half_period) @(posedge clk);
-      repeat (ser_half_period) @(posedge clk);
-      buffer = {ser_tx, buffer[7:1]};
+      repeat (ser_half_period) @(posedge r_clk);
+      repeat (ser_half_period) @(posedge r_clk);
+      buffer = {s_uart_tx, buffer[7:1]};
       ->ser_sample;  // data bit
     end
 
-    repeat (ser_half_period) @(posedge clk);
-    repeat (ser_half_period) @(posedge clk);
+    repeat (ser_half_period) @(posedge r_clk);
+    repeat (ser_half_period) @(posedge r_clk);
     ->ser_sample;  // stop bit
 
     if (buffer < 32 || buffer >= 127)
@@ -121,5 +110,16 @@ module retrosoc_tb;
       $write(
           "%c", buffer
       );
+  end
+
+  initial begin
+    $dumpfile("retrosoc_tb.vcd");
+    $dumpvars(0, retrosoc_tb);
+
+    repeat (10) begin
+      repeat (50000) @(posedge r_clk);
+      // $display("+50000 cycles");
+    end
+    // $finish;
   end
 endmodule
