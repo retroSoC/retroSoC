59e25f57091ad63ee71c0edcac0757df8b1d2694
maksyuki <maksyuki@126.com>
2025-02-12 23:03:01 +0800

    feat: add extern 8 spi fifo space

--- rtl/ip/axil_ip_wrapper.v
+++ rtl/ip/axil_ip_wrapper.v
@@ -186,7 +186,7 @@ module axil_ip_wrapper (
   );
 
   apb_spi_master #(
-      .BUFFER_DEPTH  (8),
+      .BUFFER_DEPTH  (16),
       .APB_ADDR_WIDTH(32)
   ) u_apb_spi_master (
       .HCLK    (clk_i),
