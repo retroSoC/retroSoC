0e47ce28375b524e9e34134963fad61243119dc2
maksyuki <maksyuki@126.com>
2025-03-14 20:01:05 +0800

    chore: add new ignore rule to the file and simplify the makefile

--- rtl/retrosoc_tb.v
+++ rtl/retrosoc_tb.v
@@ -199,9 +199,9 @@ module retrosoc_tb;
         $dumpfile("retrosoc_tb.fst");
         $dumpvars(0, retrosoc_tb);
       end else begin
-         $fsdbDumpfile("retrosoc_tb.fsdb");
-         $fsdbDumpvars(0);
-         $fsdbDumpMDA();
+        //  $fsdbDumpfile("retrosoc_tb.fsdb");
+        //  $fsdbDumpvars(0);
+        //  $fsdbDumpMDA();
       end
       repeat (200) begin
         repeat (5000) @(posedge r_xtal_clk);
