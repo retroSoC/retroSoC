5996db19db3a732fe917d6e1305380cb19b1fd71
maksyuki <maksyuki@126.com>
2025-03-14 19:55:56 +0800

    chore: add rtl and common comp mk scripts

---
+++ rtl/software.mk
@@ -0,0 +1,39 @@
+# software
+FIRMWARE_NAME := retrosoc_fw
+
+CROSS=riscv32-unknown-elf-
+CFLAGS := -mabi=ilp32 \
+          -march=rv32im \
+          -Wl,-Bstatic,-T,retrosoc_sections.lds,--strip-debug \
+          -ffreestanding \
+          -nostdlib
+
+SRC_PATH := ../crt/start.s \
+            ../crt/tinyuart.c \
+            ../crt/tinystring.c \
+            ../crt/tinyprintf.c \
+            ../crt/tinyflash.c \
+            ../crt/tinygpio.c \
+            ../crt/tinyhkspi.c \
+            ../crt/tinytim.c \
+            ../crt/tinyarchinfo.c \
+            ../crt/tinyrng.c \
+            ../crt/tinyhpuart.c \
+            ../crt/tinypwm.c \
+            ../crt/tinyps2.c \
+            ../crt/tinyi2c.c \
+            ../crt/tinylcd.c \
+            ../crt/tinypsram.c \
+            ../crt/tinybench.c \
+            ../crt/tinysh.c \
+            ../crt/firmware.c
+
+LDS_PATH := ../crt/sections.lds
+
+$(FIRMWARE_NAME).elf:
+	$(CROSS)cpp -P -o retrosoc_sections.lds $(LDS_PATH)
+	$(CROSS)gcc $(CFLAGS) -I../crt -o $@ $(SRC_PATH)
+	$(CROSS)objcopy -O verilog $@ $(FIRMWARE_NAME).hex
+	sed -i 's/@30000000/@00000000/g' retrosoc_fw.hex
+	$(CROSS)objcopy -O binary  $@ $(FIRMWARE_NAME).bin
+	$(CROSS)objdump -d $@ > $(FIRMWARE_NAME).txt
---
+++ rtl/vcs.mk
@@ -0,0 +1,53 @@
+NOVAS        := /nfs/tools/synopsys/verdi/V-2023.12-SP1-1/share/PLI/VCS/LINUX64
+EXTRA        := -P ${NOVAS}/novas.tab ${NOVAS}/pli.a
+
+
+SIM_TOOL     := bsub -Is vcs
+SIM_BINY     := bsub -Is ../simv
+VERDI_TOOL   := bsub -Is verdi
+
+RTL_INC  := +incdir+../ip \
+            +incdir+../ip/cust/spfs \
+            +incdir+../ip/cust/spfs_model
+
+
+RTL_FLIST := -f ../filelist/soc.fl \
+             -f ../filelist/ip.fl \
+             -f ../filelist/tech.fl \
+             -f ../filelist/pdk_s110.fl
+
+TB_FLIST  := -f ../filelist/tb.fl
+            
+
+## vcs option
+SIM_OPTIONS := -full64  +v2k -sverilog -timescale=1ns/10ps \
+                ${VEXTRA} \
+                -kdb \
+                -debug_access+all \
+                -debug_region=cell+lib \
+                +error+500 \
+                +vcs+flush+all \
+                +lint=TFIPC-L \
+                +define+no_warning \
+                +define+S50 \
+                +define+SVA_OFF \
+                -xprop=../xprop_config \
+                -work DEFAULT \
+                +define+RANDOMIZE_REG_INIT \
+                ${RTL_INC} \
+
+
+TIME_OPTION := +notimingcheck \
+               +nospecify \
+
+POST_PATH := -v /nfs/share/temp/flow_110/bes_data/sta/sdf/retrosoc_asic_CTS_MIN_CMIN_SDF_Mar_10_00/retrosoc_asic.v
+SDF_FILE  := "/nfs/share/temp/flow_110/bes_data/sta/sdf/retrosoc_asic_CTS_MIN_CMIN_SDF_Mar_10_00/retrosoc_asic_CTS_MIN.sdf.gz"
+
+POST_SIM_OPTION := -sdf min:retrosoc_tb.u_retrosoc_asic:${SDF_FILE} \
+                   +delay_mode_path \
+                   +sdfverbose \
+                   +neg_tchk \
+                   -negdelay \
+                   +optconfigfile+./disable_timing_checklist \
+                   -diag=sdf:verbose \
+                   +warn=OPD:10,IWNF:10,SDFCOM_UHICD:10,SDFCOM_ANICD:10,SDFCOM_NICD:10,DRTZ:10,SDFCOM_UHICD:10,SDFCOM_NTCDTL:10 \
