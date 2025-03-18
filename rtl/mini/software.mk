8cf6c0801801f25f263389c507e65ca1a708f44c
maksyuki <maksyuki@126.com>
2025-03-18 15:35:47 +0800

    chore: add dir to the software build files

--- rtl/mini/software.mk
+++ rtl/mini/software.mk
@@ -8,30 +8,31 @@ CFLAGS := -mabi=ilp32 \
           -ffreestanding \
           -nostdlib
 
-SRC_PATH := ../../crt/start.s \
-            ../../crt/tinyuart.c \
-            ../../crt/tinystring.c \
-            ../../crt/tinyprintf.c \
-            ../../crt/tinygpio.c \
-            ../../crt/tinytim.c \
-            ../../crt/tinyarchinfo.c \
-            ../../crt/tinyrng.c \
-            ../../crt/tinyhpuart.c \
-            ../../crt/tinypwm.c \
-            ../../crt/tinyps2.c \
-            ../../crt/tinyi2c.c \
-            ../../crt/tinylcd.c \
-            ../../crt/tinypsram.c \
-            ../../crt/tinybench.c \
-            ../../crt/tinysh.c \
-            ../../crt/firmware.c
+SRC_PATH := ../../../crt/start.s \
+            ../../../crt/tinyuart.c \
+            ../../../crt/tinystring.c \
+            ../../../crt/tinyprintf.c \
+            ../../../crt/tinygpio.c \
+            ../../../crt/tinytim.c \
+            ../../../crt/tinyarchinfo.c \
+            ../../../crt/tinyrng.c \
+            ../../../crt/tinyhpuart.c \
+            ../../../crt/tinypwm.c \
+            ../../../crt/tinyps2.c \
+            ../../../crt/tinyi2c.c \
+            ../../../crt/tinylcd.c \
+            ../../../crt/tinypsram.c \
+            ../../../crt/tinybench.c \
+            ../../../crt/tinysh.c \
+            ../../../crt/firmware.c
 
-LDS_PATH := ../../crt/sections.lds
+LDS_PATH := ../../../crt/sections.lds
 
 $(FIRMWARE_NAME).elf:
-	$(CROSS)cpp -P -o retrosoc_sections.lds $(LDS_PATH)
-	$(CROSS)gcc $(CFLAGS) -I../../crt -o $@ $(SRC_PATH)
-	$(CROSS)objcopy -O verilog $@ $(FIRMWARE_NAME).hex
-	sed -i 's/@30000000/@00000000/g' retrosoc_fw.hex
-	$(CROSS)objcopy -O binary  $@ $(FIRMWARE_NAME).bin
-	$(CROSS)objdump -d $@ > $(FIRMWARE_NAME).txt
+	@mkdir -p .sw_build
+	cd .sw_build && ($(CROSS)cpp -P -o retrosoc_sections.lds $(LDS_PATH))
+	cd .sw_build && ($(CROSS)gcc $(CFLAGS) -I../../../crt -o $@ $(SRC_PATH))
+	cd .sw_build && ($(CROSS)objcopy -O verilog $@ $(FIRMWARE_NAME).hex)
+	cd .sw_build && (sed -i 's/@30000000/@00000000/g' retrosoc_fw.hex)
+	cd .sw_build && ($(CROSS)objcopy -O binary  $@ $(FIRMWARE_NAME).bin)
+	cd .sw_build && ($(CROSS)objdump -d $@ > $(FIRMWARE_NAME).txt)
