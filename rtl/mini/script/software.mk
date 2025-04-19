e3d0f73f3867ae81f808c452443732ccbc5cd1f1
maksyuki <maksyuki@126.com>
2025-04-19 13:43:29 +0800

    chore: modify software target path include

--- rtl/mini/script/software.mk
+++ rtl/mini/script/software.mk
@@ -15,35 +15,30 @@ CFLAGS := -mabi=ilp32 \
           -ffreestanding \
           -nostdlib
 
-PRJ_ROOT_PATH = ../../..
+SRC_PATH := $(ROOT_PATH)/crt/startup.s \
+            $(ROOT_PATH)/crt/src/tinyuart.c \
+            $(ROOT_PATH)/crt/src/tinystring.c \
+            $(ROOT_PATH)/crt/src/tinyprintf.c \
+            $(ROOT_PATH)/crt/src/tinygpio.c \
+            $(ROOT_PATH)/crt/src/tinytim.c \
+            $(ROOT_PATH)/crt/src/tinyarchinfo.c \
+            $(ROOT_PATH)/crt/src/tinyrng.c \
+            $(ROOT_PATH)/crt/src/tinyhpuart.c \
+            $(ROOT_PATH)/crt/src/tinypwm.c \
+            $(ROOT_PATH)/crt/src/tinyps2.c \
+            $(ROOT_PATH)/crt/src/tinyi2c.c \
+            $(ROOT_PATH)/crt/src/tinylcd.c \
+            $(ROOT_PATH)/crt/src/tinypsram.c \
+            $(ROOT_PATH)/crt/src/tinybench.c \
+            $(ROOT_PATH)/crt/src/tinysh.c \
+            $(ROOT_PATH)/crt/src/firmware.c
 
-SRC_PATH := $(PRJ_ROOT_PATH)/crt/startup.s \
-            $(PRJ_ROOT_PATH)/crt/src/tinyuart.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinystring.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinyprintf.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinygpio.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinytim.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinyarchinfo.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinyrng.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinyhpuart.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinypwm.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinyps2.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinyi2c.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinylcd.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinypsram.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinybench.c \
-            $(PRJ_ROOT_PATH)/crt/src/tinysh.c \
-            $(PRJ_ROOT_PATH)/crt/src/firmware.c
+LDS_PATH := $(ROOT_PATH)/crt/flash_$(EXEC_TYPE).lds
 
-LDS_PATH := $(PRJ_ROOT_PATH)/crt/flash_$(EXEC_TYPE).lds
-
-# %.o: %.c %.h $(DEPS)
-# 	$(CROSS)
-
-$(FIRMWARE_NAME).elf:
+firmware:
 	@mkdir -p .sw_build
 	cd .sw_build && ($(CP) -P -o flash_$(EXEC_TYPE).lds $(LDS_PATH))
-	cd .sw_build && ($(CC) $(CFLAGS) -I$(PRJ_ROOT_PATH)/crt/inc -o $@ $(SRC_PATH))
+	cd .sw_build && ($(CC) $(CFLAGS) -I$(ROOT_PATH)/crt/inc -o $@ $(SRC_PATH))
 	cd .sw_build && ($(OBJC) -O verilog $@ $(FIRMWARE_NAME).hex)
 	cd .sw_build && ($(OBJC) -O binary $@ $(FIRMWARE_NAME).bin)
 	cd .sw_build && ($(DUMP) -d $@ > $(FIRMWARE_NAME).txt)
