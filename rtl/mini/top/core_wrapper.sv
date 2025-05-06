eb23ffac22836bfd738f68d467e1d557f266345d
maksyuki <maksyuki@126.com>
2025-05-06 09:49:44 +0800

    feat: add 'keep' keywords to reserve wrie of core wrapper

--- rtl/mini/top/core_wrapper.sv
+++ rtl/mini/top/core_wrapper.sv
@@ -1,15 +1,15 @@
 `include "mmap_define.svh"
 
 module core_wrapper (
-    input  logic        clk_i,
-    input  logic        rst_n_i,
-    output logic        core_valid_o,
-    output logic [31:0] core_addr_o,
-    output logic [31:0] core_wdata_o,
-    output logic [ 3:0] core_wstrb_o,
-    input  logic [31:0] core_rdata_i,
-    input  logic        core_ready_i,
-    input  logic [31:0] irq_i
+    (* keep *) input  logic        clk_i,
+    (* keep *) input  logic        rst_n_i,
+    (* keep *) output logic        core_valid_o,
+    (* keep *) output logic [31:0] core_addr_o,
+    (* keep *) output logic [31:0] core_wdata_o,
+    (* keep *) output logic [ 3:0] core_wstrb_o,
+    (* keep *) input  logic [31:0] core_rdata_i,
+    (* keep *) input  logic        core_ready_i,
+    (* keep *) input  logic [31:0] irq_i
 );
 
 `ifdef CORE_PICORV32
