7b85777cd2476f4b7717ee18c993bdb2ce087ac5
maksyuki <maksyuki@126.com>
2025-09-16 21:57:44 +0800

    fix: fix the ack reg in i2c0 module

--- rtl/mini/tb/retrosoc_tb.sv
+++ rtl/mini/tb/retrosoc_tb.sv
@@ -79,7 +79,7 @@ module retrosoc_tb;
       .xo_o_pad             (),
       .extclk_i_pad         (s_ext_clk),
       .audclk_i_pad         (s_aud_clk),
-      .irq_pin_i_pad        (1'b0),
+      .irq_pin_i_pad        (),
 `ifdef CORE_MDD
       .core_mdd_sel_0_i_pad (s_core_mdd_sel[0]),
       .core_mdd_sel_1_i_pad (s_core_mdd_sel[1]),
