9f8be1e2b9a2fd499fe85e731f842a1c7c08bfb2
maksyuki <maksyuki@126.com>
2025-09-21 15:30:07 +0800

    feat: add and modify ws2812 output logic impl

--- rtl/ip/native/onewire.sv
+++ rtl/ip/native/onewire.sv
@@ -10,6 +10,7 @@
 `define ONEWIRE_RSTNUM  8'h0C
 `define ONEWIRE_TXDATA  8'h10
 `define ONEWIRE_CTRL    8'h14
+`define ONEWIRE_STATUS  8'h18
 // verilog_format: on
 
 `endif
@@ -45,8 +46,15 @@ module nmi_onewire (
   logic [7:0] s_rstnum_d, s_rstnum_q;
   logic s_txdata_en;
   logic [23:0] s_txdata_d, s_txdata_q;
-  logic s_ctrl_en;
   logic [1:0] s_ctrl_d, s_ctrl_q;
+  logic [2:0] s_status_d, s_status_q;
+  // fifo
+  logic s_tx_push_valid, s_tx_empty, s_tx_full;
+  logic s_tx_pop_valid, s_tx_pop_ready;
+  logic [23:0] s_tx_push_data, s_tx_pop_data;
+
+  logic s_done;
+
 
 
   assign s_nmi_wr_hdshk = nmi.valid && (~s_nmi_ready_q) && (|nmi.wstrb);
@@ -95,23 +103,107 @@ module nmi_onewire (
       s_rstnum_q
   );
 
-  // TXDATA
+  always_comb begin
+    s_tx_push_valid = 1'b0;
+    s_tx_push_data  = '0;
+    if (s_nmi_wr_hdshk && nmi.addr[7:0] == `ONEWIRE_TXDATA) begin
+      s_tx_push_valid = 1'b1;
+      s_tx_push_data  = nmi.wdata[23:0];
+    end
+  end
 
-  assign s_ctrl_en = s_nmi_wr_hdshk && nmi.addr[7:0] == `ONEWIRE_CTRL;
+  assign s_tx_pop_ready = ~s_tx_empty;
+  fifo #(
+      .DATA_WIDTH  (24),
+      .BUFFER_DEPTH(8)
+  ) u_tx_fifo (
+      .clk_i  (clk_i),
+      .rst_n_i(rst_n_i),
+      .flush_i(s_ctrl_q[0]),
+      .cnt_o  (),
+      .push_i (s_tx_push_valid),
+      .full_o (s_tx_full),
+      .dat_i  (s_tx_push_data),
+      .pop_i  (s_tx_pop_valid),
+      .empty_o(s_tx_empty),
+      .dat_o  (s_tx_pop_data)
+  );
 
-  assign s_ctrl_d  = nmi.wdata[1:0];
-  dffer #(2) u_ctrl_dffer (
+
+  // [0] clear fifo [1] start
+  always_comb begin
+    if (s_nmi_wr_hdshk && nmi.addr[7:0] == `ONEWIRE_CTRL) begin
+      s_ctrl_d = nmi.wdata[1:0];
+    end else begin
+      s_ctrl_d = '0;
+    end
+  end
+  dffr #(2) u_ctrl_dffr (
       clk_i,
       rst_n_i,
-      s_ctrl_en,
       s_ctrl_d,
       s_ctrl_q
   );
 
 
+  // [0] xfer done [1] fifo full, [2] fifo empty
+  always_comb begin
+    s_status_d    = s_status_q;
+    s_status_d[1] = s_tx_full;
+    s_status_d[2] = s_tx_empty;
+    if (s_done) begin
+      s_status_d[0] = 1'b1;
+    end else if (s_nmi_rd_hdshk && nmi.addr[7:0] == `ONEWIRE_STATUS) begin
+      s_status_d[0] = 1'b0;
+    end
+  end
+  dffr #(3) u_status_dffr (
+      clk_i,
+      rst_n_i,
+      s_status_d,
+      s_status_q
+  );
+
+  assign s_nmi_ready_d = nmi.valid && (~s_nmi_ready_q);
+  dffr #(1) u_nmi_ready_dffr (
+      clk_i,
+      rst_n_i,
+      s_nmi_ready_d,
+      s_nmi_ready_q
+  );
+
+  assign s_nmi_rdata_en = s_nmi_rd_hdshk;
+  always_comb begin
+    s_nmi_rdata_d = s_nmi_rdata_q;
+    unique case (nmi.addr[7:0])
+      `ONEWIRE_CLKDIV:  s_nmi_rdata_d = {24'd0, s_clkdiv_q};
+      `ONEWIRE_ZEROCNT: s_nmi_rdata_d = {24'd0, s_zerocnt_q};
+      `ONEWIRE_ONECNT:  s_nmi_rdata_d = {24'd0, s_onecnt_q};
+      `ONEWIRE_RSTNUM:  s_nmi_rdata_d = {24'd0, s_rstnum_q};
+      `ONEWIRE_STATUS:  s_nmi_rdata_d = {31'd0, s_status_q};
+      default:          s_nmi_rdata_d = s_nmi_rdata_q;
+    endcase
+  end
+  dffer #(32) u_nmi_rdata_dffer (
+      clk_i,
+      rst_n_i,
+      s_nmi_rdata_en,
+      s_nmi_rdata_d,
+      s_nmi_rdata_q
+  );
+
   onewire_core u_onewire_core (
-      .clk_i  (clk_i),
-      .rst_n_i(rst_n_i),
-      .onewire(onewire)
+      .clk_i     (clk_i),
+      .rst_n_i   (rst_n_i),
+      .clkdiv_i  (s_clkdiv_q),
+      .zerocnt_i (s_zerocnt_q),
+      .onecnt_i  (s_onecnt_q),
+      .rstnum_i  (s_rstnum_q),
+      .start_i   (s_ctrl_q[0]),
+      .data_req_o(s_tx_pop_valid),
+      .data_rdy_i(s_tx_pop_ready),
+      .data_i    (s_tx_pop_data),
+      .done_o    (s_done),
+      .onewire   (onewire)
   );
 endmodule
--- rtl/ip/native/onewire_core.sv
+++ rtl/ip/native/onewire_core.sv
@@ -1,10 +1,137 @@
+
+
 module onewire_core (
     // verilog_format: off
-    input logic    clk_i,
-    input logic    rst_n_i,
-    onewire_if.dut onewire
+    input  logic        clk_i,
+    input  logic        rst_n_i,
+    input  logic [7:0]  clkdiv_i,
+    input  logic [7:0]  zerocnt_i,
+    input  logic [7:0]  onecnt_i,
+    input  logic [7:0]  rstnum_i,
+    input  logic        start_i,
+    output logic        data_req_o,
+    input  logic        data_rdy_i,
+    input  logic [23:0] data_i,
+    output logic        done_o,
+    onewire_if.dut      onewire
     // verilog_format: on
 );
 
-  assign onewire.dat_o = '0;
+  localparam FSM_IDLE = 2'd0;
+  localparam FSM_XFER = 2'd1;
+  localparam FSM_RST = 2'd2;
+
+  logic s_clkdiv_cnt_en;
+  logic [7:0] s_clkdiv_cnt_d, s_clkdiv_cnt_q;
+  logic [1:0] s_fsm_d, s_fsm_q;
+  logic [4:0] s_bit_cnt_d, s_bit_cnt_q;
+  logic [7:0] s_rst_cnt_d, s_rst_cnt_q;
+  logic [23:0] s_xfer_data_d, s_xfer_data_q;
+  logic s_done_d, s_done_q;
+
+  assign done_o = s_done_q;
+
+  always_comb begin
+    s_clkdiv_cnt_d = s_clkdiv_cnt_q;
+    s_fsm_d        = s_fsm_q;
+    s_done_d       = s_done_q;
+    s_bit_cnt_d    = s_bit_cnt_q;
+    s_rst_cnt_d    = s_rst_cnt_q;
+    s_xfer_data_d  = s_xfer_data_q;
+    onewire.dat_o  = '0;
+    unique case (s_fsm_q)
+      FSM_IDLE: begin
+        s_done_d = 1'b0;
+        if (start_i && data_rdy_i) begin
+          data_req_o    = 1'b1;
+          s_xfer_data_d = data_i;
+          s_fsm_d       = FSM_XFER;
+        end
+      end
+      FSM_XFER: begin
+        // handle the data
+        // xfer '1'
+        if (s_xfer_data_q[23]) begin
+          if (s_clkdiv_cnt_q <= onecnt_i) onewire.dat_o = 1'b1;
+          else onewire.dat_o = 1'b0;
+        end else begin
+          if (s_clkdiv_cnt_q <= zerocnt_i) onewire.dat_o = 1'b1;
+          else onewire.dat_o = 1'b0;
+        end
+        s_clkdiv_cnt_d = s_clkdiv_cnt_q + 1'b1;
+        if (s_clkdiv_cnt_q == clkdiv_i) begin
+          s_clkdiv_cnt_d = '0;
+          s_xfer_data_d  = {s_xfer_data_q[22:0], 1'b0};
+          s_bit_cnt_d    = s_bit_cnt_q + 1'b1;
+        end
+
+        if (s_bit_cnt_q == 5'd23) begin
+          s_bit_cnt_d = '0;
+          if (~data_rdy_i) begin
+            s_fsm_d = FSM_RST;
+          end else begin
+            data_req_o    = 1'b1;
+            s_xfer_data_d = data_i;
+          end
+        end
+      end
+      FSM_RST: begin
+        if (s_rst_cnt_q == rstnum_i) begin
+          s_rst_cnt_d = '0;
+          s_fsm_d     = FSM_IDLE;
+          s_done_d    = 1'b1;
+        end else begin
+          s_rst_cnt_d = s_rst_cnt_q + 1'b1;
+        end
+      end
+    endcase
+  end
+  dffr #(2) u_fsm_dffr (
+      clk_i,
+      rst_n_i,
+      s_fsm_d,
+      s_fsm_q
+  );
+
+  assign s_clkdiv_cnt_en = s_fsm_q == FSM_XFER;
+  dffer #(8) u_clkdiv_cnt_dffer (
+      clk_i,
+      rst_n_i,
+      s_clkdiv_cnt_en,
+      s_clkdiv_cnt_d,
+      s_clkdiv_cnt_q
+  );
+
+  dffr #(1) u_done_dffr (
+      clk_i,
+      rst_n_i,
+      s_done_d,
+      s_done_q
+  );
+
+  dffr #(5) u_bit_cnt_dffr (
+      clk_i,
+      rst_n_i,
+      s_bit_cnt_d,
+      s_bit_cnt_q
+  );
+
+  dffr #(8) u_rst_cnt_dffr (
+      clk_i,
+      rst_n_i,
+      s_rst_cnt_d,
+      s_rst_cnt_q
+  );
+
+  dffr #(24) u_xfer_data_dffr (
+      clk_i,
+      rst_n_i,
+      s_xfer_data_d,
+      s_xfer_data_q
+  );
+
+
+
+
+
 endmodule
