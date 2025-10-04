bf9bd9b251ae2eb2753f534fa9fb59329d0104de
maksyuki <maksyuki@126.com>
2025-10-04 10:49:04 +0800

    feat: add 32b support for perf improvement

--- rtl/ip/native/i2s_core.sv
+++ rtl/ip/native/i2s_core.sv
@@ -14,10 +14,10 @@ module i2s_core (
     input  logic        rst_n_i,
     input  logic        mode_i,
     output logic        tx_valid_o,
-    input  logic [15:0] tx_data_i,
+    input  logic [31:0] tx_data_i,
     input  logic        tx_empty_i,
     output logic        rx_valid_o,
-    output logic [15:0] rx_data_o,
+    output logic [31:0] rx_data_o,
     input  logic        rx_full_i,
     nv_i2s_if.dut       i2s
     // verilog_format: on
@@ -32,32 +32,79 @@ module i2s_core (
   logic [15:0] s_loopback_data;
 
   logic [15:0] s_recv_data, s_send_data;
+  logic [15:0] s_recv_data_d, s_recv_data_q;
+  logic [15:0] s_send_data_d, s_send_data_q;
   logic s_recv_done, s_send_done;
+  logic s_recv_cnt_d, s_recv_cnt_q;
+  logic s_send_cnt_d, s_send_cnt_q;
 
   always_comb begin
     rx_valid_o      = '0;
     rx_data_o       = '0;
     s_loopback_data = '0;
+    s_recv_cnt_d    = s_recv_cnt_q;
+    s_recv_data_d   = s_recv_data_q;
     if (!mode_i) s_loopback_data = s_recv_data;
     else begin
-      if (~rx_full_i && s_recv_done) begin
-        rx_valid_o = 1'b1;
-        rx_data_o  = s_recv_data;
+      if (s_recv_done) begin
+        if (~rx_full_i && s_recv_cnt_q == '1) begin
+          s_recv_cnt_d = '0;
+          rx_valid_o   = 1'b1;
+          rx_data_o    = {s_recv_data, s_recv_data_q};
+        end else begin
+          s_recv_cnt_d  = s_recv_cnt_q + 1'b1;
+          s_recv_data_d = s_recv_data;
+        end
       end
     end
   end
+  dffr #(1) u_recv_cnt_dffr (
+      clk_i,
+      rst_n_i,
+      s_recv_cnt_d,
+      s_recv_cnt_q
+  );
+  dffr #(16) u_recv_data_dffr (
+      clk_i,
+      rst_n_i,
+      s_recv_data_d,
+      s_recv_data_q
+  );
+
 
   always_comb begin
-    tx_valid_o  = '0;
-    s_send_data = '0;
+    tx_valid_o    = '0;
+    s_send_data   = s_send_data_q;
+    s_send_cnt_d  = s_send_cnt_q;
+    s_send_data_d = s_send_data_q;
     if (!mode_i) s_send_data = s_loopback_data;
     else begin
-      if (~tx_empty_i && s_send_done) begin
-        tx_valid_o  = 1'b1;
-        s_send_data = tx_data_i;
+      if (s_send_done) begin
+        if (s_send_cnt_q == 1'b1) begin
+          s_send_cnt_d = '0;
+          s_send_data  = s_send_data_q;
+        end else if (~tx_empty_i) begin
+          tx_valid_o    = 1'b1;
+          s_send_cnt_d  = s_send_cnt_q + 1'b1;
+          s_send_data   = tx_data_i[15:0];
+          s_send_data_d = tx_data_i[31:16];
+        end
       end
     end
   end
+  dffr #(1) u_send_cnt_dffr (
+      clk_i,
+      rst_n_i,
+      s_send_cnt_d,
+      s_send_cnt_q
+  );
+  dffr #(16) u_send_data_dffr (
+      clk_i,
+      rst_n_i,
+      s_send_data_d,
+      s_send_data_q
+  );
+
 
   assign i2s.mclk_o  = clk_i;
   assign i2s.sclk_o  = s_sclk_q;
