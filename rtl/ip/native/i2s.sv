35efde3d0aaedc0b4b480f8ea00771132db3d364
maksyuki <maksyuki@126.com>
2025-10-07 21:28:41 +0800

    feat: add i2s rx dma trg func and fix rxdata rd data valid error

--- rtl/ip/native/i2s.sv
+++ rtl/ip/native/i2s.sv
@@ -15,9 +15,10 @@
 `define NATV_I2S_MODE     8'h00
 `define NATV_I2S_UPBOUND  8'h04
 `define NATV_I2S_LOWBOUND 8'h08
-`define NATV_I2S_TXDATA   8'h0C
-`define NATV_I2S_RXDATA   8'h10
-`define NATV_I2S_STATUS   8'h14
+`define NATV_I2S_RECVEN   8'h0C
+`define NATV_I2S_TXDATA   8'h10
+`define NATV_I2S_RXDATA   8'h14
+`define NATV_I2S_STATUS   8'h18
 // verilog_format: on
 
 interface nv_i2s_if ();
@@ -40,6 +41,7 @@ module nmi_i2s (
     input logic   clk_aud_i  ,
     input logic   rst_aud_n_i,
     output logic  dma_tx_stall_o,
+    output logic  dma_rx_stall_o,
     nmi_if.slave  nmi,
     nv_i2s_if.dut i2s
     // verilog_format: on
@@ -56,6 +58,8 @@ module nmi_i2s (
   logic [7:0] s_i2s_upbound_d, s_i2s_upbound_q;
   logic s_i2s_lowbound_en;
   logic [7:0] s_i2s_lowbound_d, s_i2s_lowbound_q;
+  logic s_i2s_recven_en;
+  logic s_i2s_recven_d, s_i2s_recven_q;
   logic [1:0] s_i2s_status_d, s_i2s_status_q;
   // tx fifo
   logic s_tx_push_valid, s_tx_full, s_tx_empty;
@@ -66,8 +70,10 @@ module nmi_i2s (
   logic s_rx_push_valid, s_rx_full, s_rx_empty;
   logic s_rx_pop_valid, s_rx_pop_ready;
   logic [31:0] s_rx_push_data, s_rx_pop_data;
+  logic [7:0] s_rx_elem_num;
   // common
   logic s_tx_fifo_stall_d, s_tx_fifo_stall_q;
+  logic s_rx_fifo_stall_d, s_rx_fifo_stall_q;
 
   assign s_nmi_wr_hdshk = nmi.valid && (~s_nmi_ready_q) && (|nmi.wstrb);
   assign s_nmi_rd_hdshk = nmi.valid && (~s_nmi_ready_q) && (~(|nmi.wstrb));
@@ -75,6 +81,7 @@ module nmi_i2s (
   assign nmi.rdata      = s_nmi_rdata_q;
 
   assign dma_tx_stall_o = s_tx_fifo_stall_q;
+  assign dma_rx_stall_o = s_rx_fifo_stall_q;
 
   assign s_i2s_mode_en  = s_nmi_wr_hdshk && nmi.addr[7:0] == `NATV_I2S_MODE;
   assign s_i2s_mode_d   = nmi.wdata[0];
@@ -108,6 +115,17 @@ module nmi_i2s (
   );
 
 
+  assign s_i2s_recven_en = s_nmi_wr_hdshk && nmi.addr[7:0] == `NATV_I2S_RECVEN;
+  assign s_i2s_recven_d  = nmi.wdata[0];
+  dffer #(1) u_i2s_recven_dffer (
+      clk_i,
+      rst_n_i,
+      s_i2s_recven_en,
+      s_i2s_recven_d,
+      s_i2s_recven_q
+  );
+
+
   always_comb begin
     s_tx_fifo_stall_d = s_tx_fifo_stall_q;
     if (~s_tx_fifo_stall_q && s_tx_elem_num > s_i2s_upbound_q) begin
@@ -124,6 +142,20 @@ module nmi_i2s (
   );
 
 
+  always_comb begin
+    s_rx_fifo_stall_d = s_rx_fifo_stall_q;
+    if (~s_rx_fifo_stall_q && s_rx_elem_num < s_i2s_lowbound_q) begin
+      s_rx_fifo_stall_d = 1'b1;
+    end else if (s_rx_fifo_stall_q && s_rx_elem_num > s_i2s_upbound_q) begin
+      s_rx_fifo_stall_d = 1'b0;
+    end
+  end
+  dffr #(1) u_rx_fifo_stall_dffr (
+      clk_i,
+      rst_n_i,
+      s_rx_fifo_stall_d,
+      s_rx_fifo_stall_q
+  );
 
 
   // TODO: need to handle when tx fifo is full(DMA is fine)
@@ -166,9 +198,11 @@ module nmi_i2s (
     unique case (nmi.addr[7:0])
       `NATV_I2S_MODE:   s_nmi_rdata_d = {31'd0, s_i2s_mode_q};
       `NATV_I2S_RXDATA: begin
-        s_rx_pop_valid = 1'b1;
-        if (!s_rx_empty) s_nmi_rdata_d = s_rx_pop_data;
-        else s_nmi_rdata_d = '0;
+        if (s_nmi_rd_hdshk) begin
+          s_rx_pop_valid = 1'b1;
+          if (!s_rx_empty) s_nmi_rdata_d = s_rx_pop_data;
+          else s_nmi_rdata_d = '0;
+        end
       end
       `NATV_I2S_STATUS: s_nmi_rdata_d = {30'd0, s_i2s_status_q};
       default:          s_nmi_rdata_d = s_nmi_rdata_q;
@@ -207,7 +241,7 @@ module nmi_i2s (
   ) u_rx_async_fifo (
       .wr_clk_i  (clk_aud_i),
       .wr_rst_n_i(rst_aud_n_i),
-      .wr_en_i   (s_rx_push_valid),
+      .wr_en_i   (s_rx_push_valid & s_i2s_recven_q),
       .wr_data_i (s_rx_push_data),
       .wr_full_o (s_rx_full),
       .rd_clk_i  (clk_i),
@@ -215,7 +249,7 @@ module nmi_i2s (
       .rd_en_i   (s_rx_pop_valid),
       .rd_data_o (s_rx_pop_data),
       .rd_empty_o(s_rx_empty),
-      .elem_num_o()
+      .elem_num_o(s_rx_elem_num)
   );
 
   i2s_core u_i2s_core (
