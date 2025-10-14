02ef34b212099fa8ce7b09fafe09987992a674d9
maksyuki <maksyuki@126.com>
2025-10-14 15:37:58 +0800

    feat: add i2s wav audio test for tiny shell module

--- crt/inc/tinyuart.h
+++ crt/inc/tinyuart.h
@@ -2,5 +2,6 @@
 #ifndef TINYUART_H__
 #define TINYUART_H__
 
+void uart0_init(uint32_t freq, uint32_t bps);
 void putch(char ch);
 #endif
--- crt/src/tinyi2s.c
+++ crt/src/tinyi2s.c
@@ -1,8 +1,10 @@
 #include <firmware.h>
+#include <tinyprint.h>
 #include <tinyprintf.h>
-#include <wav_decoder.h>
 #include <tinydma.h>
 #include <tinyi2s.h>
+#include <wav_audio.h>
+#include <es8388.h>
 
 uint32_t test_data[] = {0x12345678, 0x12345679, 0x1234567A, 0x2345EF23, 0x2345EF24, 0x2345EF25};
 
@@ -33,13 +35,37 @@ void i2s_simp_test() {
 }
 
 void ip_i2s_test() {
-    // fifo wr mode
+    char type_ch;
+    uint32_t mode = 0;
+
+    es8388_init();
     printf("[APB IP] i2s test\n");
-    i2s_init(1);
-    // wav_file_decoder((uint32_t)0x51004000);
-    wav_file_decoder((uint32_t)0x54737000);
-    // dma_config(1, (uint32_t)audio_data, (uint32_t)1, (uint32_t)&reg_i2s_txdata, (uint32_t)0, 1024 * 512 / 4);
-    // dma_start_xfer();
-    // dma_wait_done();
+    printf("exit[e] mode[m] start[s] stop[t] reset[r] \n");
+
+    while(1) {
+        type_ch = getchar();
+        if(type_ch == 'e') break;
+        else if(type_ch == 'm') {
+            if(mode) {
+                mode = 0;
+                printf("switch to loopback mode\n");
+            } else {
+                mode = 1;
+                printf("switch to normal mode\n");
+            }
+            i2s_init(mode);
+        } else if(type_ch == 's') {
+            if(mode == 1) wav_audio_play((uint32_t)0x54737000);
+            else printf("need to set normal mode first\n");
+        } else if(type_ch == 't') {
+            i2s_init(0);
+            dma_stop_xfer();
+        } else if(type_ch == 'r') {
+            i2s_init(0);
+            dma_reset_xfer();
+        }
+    }
+
+    // wav_audio_play((uint32_t)0x51004000);
     i2s_init(0);
 }
