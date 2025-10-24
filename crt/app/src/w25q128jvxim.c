1dc56c419d57c74164595bbed632100f1333a993
maksyuki <maksyuki@126.com>
2025-10-24 22:10:19 +0800

    feat: modify qspi wr/rd test impl

--- crt/app/src/w25q128jvxim.c
+++ crt/app/src/w25q128jvxim.c
@@ -24,6 +24,16 @@ static void norflash_spi_wr_data(uint32_t* data, uint32_t len) {
              data);
 }
 
+// 1-1-4(32h+3B-adr)
+static void norflash_qspi_wr_data(uint32_t* data, uint32_t len) {
+    printf("len: %d\n", len);
+    qspi0_wr((uint32_t)1, (uint32_t)1, (uint32_t)0x32000000,
+             (uint32_t)1, (uint32_t)3, (uint32_t)0x00000000,
+             (uint32_t)0,
+             (uint32_t)3, len, (uint32_t)4,
+             data);
+}
+
 
 static void norflash_spi_rd_data(uint32_t len) {
     uint32_t data[64] = {0};
@@ -67,6 +77,31 @@ static void norflash_dspi_rd_data(uint32_t len) {
              (uint32_t)2, (uint32_t)4, (uint32_t)0x000000F0,
              (uint32_t)0,
              (uint32_t)2, len, (uint32_t)4,
+             data);
+
+    printf("rd: ");
+    for(uint32_t i = 0; i < len; ++i) printf(" %x", data[i]);
+    printf("\n");
+}
+
+static void norflash_qspi_rd_data(uint32_t len) {
+    printf("len: %d", len);
+    uint32_t data[64] = {0};
+
+    qspi0_rd((uint32_t)1, (uint32_t)1, (uint32_t)0x6B000000,
+             (uint32_t)1, (uint32_t)3, (uint32_t)0x00000000,
+             (uint32_t)8,
+             (uint32_t)3, len, (uint32_t)4,
+             data);
+
+    printf("rd: ");
+    for(uint32_t i = 0; i < len; ++i) printf(" %x", data[i]);
+    printf("\n");
+
+    qspi0_rd((uint32_t)1, (uint32_t)1, (uint32_t)0xEB000000,
+             (uint32_t)3, (uint32_t)4, (uint32_t)0x000000F0,
+             (uint32_t)4,
+             (uint32_t)3, len, (uint32_t)4,
              data);
 
     printf("rd: ");
@@ -130,7 +165,8 @@ static void norflash_spi_status() {
         qspi0_rd((uint32_t)1, (uint32_t)1, cmd[i],
                  (uint32_t)0, (uint32_t)0, (uint32_t)0x00000000,
                  (uint32_t)0,
-                 (uint32_t)1, (uint32_t)1, (uint32_t)1, data);
+                 (uint32_t)1, (uint32_t)1, (uint32_t)1,
+                 data);
         printf("status reg %d: %x\n", i, data[0]);
     }
 }
@@ -143,20 +179,21 @@ static void norflash_spi_status() {
 static void norflash_enter_quad_mode() {
     uint32_t data[] = {0};
 
-
     qspi0_rd((uint32_t)1, (uint32_t)1, (uint32_t)0x35000000,
              (uint32_t)0, (uint32_t)0, (uint32_t)0x00000000,
              (uint32_t)0,
-             (uint32_t)1, (uint32_t)1, (uint32_t)1, data);
+             (uint32_t)1, (uint32_t)1, (uint32_t)1,
+             data);
 
     // set QE=1 in status reg 2 for QSPI/QPI
-    data[0] = data[0] | 0x02;
+    data[0] = (data[0] | 0x02) << 24;
+    printf("data[0]: %x\n", data[0]);
 
     norflash_spi_wr_en();
     qspi0_wr((uint32_t)1, (uint32_t)1, (uint32_t)0x31000000,
-             (uint32_t)0, (uint32_t)3, (uint32_t)0x00000000,
+             (uint32_t)0, (uint32_t)0, (uint32_t)0x00000000,
              (uint32_t)0,
-             (uint32_t)1, (uint32_t)1, (uint32_t)2,
+             (uint32_t)1, (uint32_t)1, (uint32_t)1,
              data);
 
     norflash_spi_status();
@@ -167,7 +204,7 @@ static void norflash_enter_quad_mode() {
 // }
 
 static void norflash_spi_test() {
-    printf("norflash spi test\n");
+    printf("\nnorflash spi test\n");
     uint32_t wr_data[] = {0x23456788, 0x00FFEE30, 0x418461AB, 0xC252DE21, 0x00550022, 0x00002266};
 
     norflash_spi_id();
@@ -179,7 +216,7 @@ static void norflash_spi_test() {
 }
 
 static void norflash_dspi_test() {
-    printf("norflash dspi test\n");
+    printf("\nnorflash dspi test\n");
     uint32_t wr_data[] = {0x23456788, 0x00FFEE30, 0x418461AB, 0xC252DE21, 0x00550022, 0x00002266};
 
     norflash_dspi_id();
@@ -190,15 +227,16 @@ static void norflash_dspi_test() {
 }
 
 static void norflash_qspi_test() {
-    printf("norflash qspi test\n");
-    norflash_enter_quad_mode();
-    norflash_qspi_id();
+    printf("\nnorflash qspi test\n");
+    uint32_t wr_data[] = {0x23456788, 0x00FFEE30, 0x418461AB, 0xC252DE21, 0x00550022, 0x00002266};
 
-    // qspi0_spi_wr_cmd(0x06);
-    // qspi0_qspi_wr_data(val_list, len); // 1-1-4(32h+3B-adr)
-    // rd: 
-    // qspi0_qspi_rd_data(val_list, dumlen=8, datalen); // 1-1-4(6Bh+3B-adr+1B-dum)
-    // qspi0_qspi_rd_data(val_list, dumlen=4, datalen); // 1-4-4(EBh+4B-adr+1/2B-dum, lowest byte need to set Fxh)
+    norflash_enter_quad_mode();
+    // norflash_qspi_id();
+
+    norflash_spi_wr_en();
+    norflash_qspi_wr_data(wr_data, sizeof(wr_data)/sizeof(wr_data[0]));
+    // rd:
+    norflash_qspi_rd_data(sizeof(wr_data)/sizeof(wr_data[0]));
 }
 
 // static void norflash_qpi_test() {
@@ -227,7 +265,7 @@ void ip_norflash_test() {
     qspi0_init(qspi0);
 
     // norflash_spi_test();
-    norflash_dspi_test();
+    // norflash_dspi_test();
     norflash_qspi_test();
     // norflash_qpi_test();
 }
