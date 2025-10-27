53cf1833de7f8dd4409df55f444ec9a551c65be7
maksyuki <maksyuki@126.com>
2025-10-27 23:10:32 +0800

    feat: add and modify tim delay val and qspi impl

--- crt/src/firmware.c
+++ crt/src/firmware.c
@@ -3,67 +3,12 @@
 void main()
 {
     uart0_init(CPU_FREQ, UART_BPS);
-
-    // reg_qspi0_mode = (uint32_t)0;
-    // reg_qspi0_clkdiv = (uint32_t)0;
-    // reg_qspi0_nss = (uint32_t)0b0001;
-    // reg_qspi0_txupbound = (uint32_t)250;
-    // reg_qspi0_txlowbound = (uint32_t)140;
-    // reg_qspi0_cmdtyp = (uint32_t)1;
-    // reg_qspi0_cmdlen = (uint32_t)1;
-    // reg_qspi0_cmddat = (uint32_t)0xA4000000;
-    // reg_qspi0_adrtyp = (uint32_t)1;
-    // reg_qspi0_adrlen = (uint32_t)3;
-    // reg_qspi0_adrdat = (uint32_t)0x12345600;
-    // reg_qspi0_dumtyp = (uint32_t)0;
-    // reg_qspi0_dumlen = (uint32_t)0;
-    // reg_qspi0_dumdat = (uint32_t)0;
-    // reg_qspi0_dattyp = (uint32_t)1;
-    // reg_qspi0_datlen = (uint32_t)2;
-    // reg_qspi0_hlvlen = (uint32_t)3;
-    // // wr data
-    // reg_qspi0_txdata = (uint32_t)0x23456789;
-    // reg_qspi0_txdata = (uint32_t)0x67674545;
-    // reg_qspi0_start = (uint32_t)1;
-    // while((reg_qspi0_status & (uint32_t)1) == 0);
-
-    // reg_qspi0_txdata = (uint32_t)0x57572323;
-    // reg_qspi0_txdata = (uint32_t)0x87654321;
-    // reg_qspi0_txdata = (uint32_t)0x12345678;
-    // reg_qspi0_datlen = (uint32_t)3;
-    // reg_qspi0_start = (uint32_t)1;
-    // while((reg_qspi0_status & (uint32_t)1) == 0);
-
-    // // printf("dma test\n");
-    // reg_qspi0_mode = (uint32_t)1;
-    // reg_qspi0_clkdiv = (uint32_t)0;
-    // reg_qspi0_nss = (uint32_t)0b0001;
-    // reg_qspi0_txupbound = (uint32_t)250;
-    // reg_qspi0_txlowbound = (uint32_t)140;
-    // reg_qspi0_cmdtyp = (uint32_t)0;
-    // reg_qspi0_cmdlen = (uint32_t)1;
-    // reg_qspi0_cmddat = (uint32_t)0xA4000000;
-    // reg_qspi0_adrtyp = (uint32_t)0;
-    // reg_qspi0_adrlen = (uint32_t)3;
-    // reg_qspi0_adrdat = (uint32_t)0x12345600;
-    // reg_qspi0_dumtyp = (uint32_t)0;
-    // reg_qspi0_dumlen = (uint32_t)0;
-    // reg_qspi0_dumdat = (uint32_t)0;
-    // reg_qspi0_dattyp = (uint32_t)1;
-    // reg_qspi0_datlen = (uint32_t)1;
-    // reg_qspi0_hlvlen = (uint32_t)2;
-    // // dma config
-    // reg_dma_mode = (uint32_t)3; // qspi tx fifo
-    // reg_dma_srcaddr = (uint32_t)0x40000000;
-    // reg_dma_srcincr = (uint32_t)1;
-    // reg_dma_dstaddr = (uint32_t)(&reg_qspi0_txdata);
-    // reg_dma_dstincr = (uint32_t)0;
-    // reg_dma_xferlen = (uint32_t)32;
-    // reg_dma_start = (uint32_t)1;
-    // while(reg_dma_status == (uint32_t)0);
-    // printf("dma tx done\n");
-    ip_lcd_test();
-    while(1);
+    // printf("bef delay\n");
+    // delay_ms(2);
+    // printf("aft delay\n");
+    // while(1);
+    // ip_lcd_test();
+    // while(1);
     i2c0_init((uint8_t)(CPU_FREQ / 2 - 1));
     app_system_boot();
     ip_archinfo_test();
@@ -75,7 +20,7 @@ void main()
     ip_wdg_test();
     ip_rng_test();
     ip_crc_test();
-    ip_norflash_test();
+    // ip_norflash_test();
     // ip_hpuart_test();
     // ip_ps2_test();
     ip_lcd_test();
