96c5dce5723e4fe532fb5f531c56e61b37a6cc5e
maksyuki <maksyuki@126.com>
2025-12-18 10:36:16 +0800

    feat: add verilator inc path modify script

---
+++ verilator.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import os
+
+ROOT_PATH = os.getcwd()
+print(f'ROOT_PATH: {ROOT_PATH}')
+
+lines = ''
+# remove parallel case statement
+with open(f'{ROOT_PATH}/rtl/mini/filelist/inc.fl', 'r', encoding='utf-8') as fp:
+    for v in fp:
+        if '+incdir+..' in v:
+            lines += v.replace('+incdir+..', f'+incdir+{ROOT_PATH}')
+        else:
+            lines += v
+
+with open(f'{ROOT_PATH}/rtl/mini/filelist/inc_verilator.fl', 'w', encoding='utf-8') as fp:
+    fp.writelines(lines)
