289342a802fd5a5efd74b6643ed673242ae98f84
maksyuki <maksyuki@126.com>
2025-12-18 11:04:27 +0800

    fix: fix some typos

--- pdk/get_pdk.py
+++ pdk/get_pdk.py
@@ -6,4 +6,4 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 os.system(f'git clone --recursive https://github.com/IHP-GmbH/IHP-Open-PDK.git {SCRIPT_DIR}/IHP-Open-PDK')
-os.sysetm(f'cd {SCRIPT_DIR}/IHP-Open-PDK && git checkout 68eebafcd9b2f5e92c69d37a8d3d90eb266550f5')
+os.system(f'cd {SCRIPT_DIR}/IHP-Open-PDK && git checkout 68eebafcd9b2f5e92c69d37a8d3d90eb266550f5')
