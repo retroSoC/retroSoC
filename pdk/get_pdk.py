8b2e85125bc00418ad999b638625e0ec18f27f0c
maksyuki <maksyuki@126.com>
2025-12-18 10:54:13 +0800

    feat: add IHP PDK repo support

--- pdk/get_pdk.py
+++ pdk/get_pdk.py
@@ -2,4 +2,8 @@
 
 import os
 
-os.system('git clone --recursive git@github.com:IHP-GmbH/IHP-Open-PDK.git')
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+os.system(f'git clone --recursive https://github.com/IHP-GmbH/IHP-Open-PDK.git {SCRIPT_DIR}/IHP-Open-PDK')
+os.sysetm(f'cd {SCRIPT_DIR}/IHP-Open-PDK && git checkout 68eebafcd9b2f5e92c69d37a8d3d90eb266550f5')
