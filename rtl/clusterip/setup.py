d90e222e0bfa47ef9f3bf4089c533a182acfb2d2
maksyuki <maksyuki@126.com>
2025-12-18 11:44:06 +0800

    feat: add clusterIP setup script

--- rtl/clusterip/setup.py
+++ rtl/clusterip/setup.py
@@ -2,8 +2,10 @@
 
 import os
 
-ip = ['archinfo', 'common', 'i2c', 'pwm', 'ps2', 'rng', 'uart', 'spi', 'rtc', 'wdg', 'crc', 'timer']
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ip = ['archinfo', 'common', 'crc', 'i2c', 'ps2', 'pwm', 'rng', 'rtc', 'spi', 'timer', 'uart', 'wdg']
 
 for v in ip:
     # print(f'git clone git@github.com:retroSoC/{v}.git')
-    os.system(f'git clone git@github.com:retroSoC/{v}.git')
+    os.system(f'git clone https://github.com/retroSoC/{v}.git {SCRIPT_DIR}/{v}')
