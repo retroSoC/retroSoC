4ded6c3e884fdc605da1fbe6e265a0b23dafdbbf
maksyuki <maksyuki@126.com>
2026-01-12 16:33:04 +0800

    feat: change default clock freq from '48MHz' to '72MHz'

--- crt/inc/firmware.h
+++ crt/inc/firmware.h
@@ -12,7 +12,7 @@
 #define HW_CORE "none"
 #endif
 
-#define CPU_FREQ            48     // unit: MHz
+#define CPU_FREQ            72     // unit: MHz
 #define UART_BPS            921600 // unit: bps
 #define PSRAM_NUM           4
 #define PSRAM_SCLK_MIN_FREQ 12     // unit: MHz
