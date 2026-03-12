3bf87bcbf91c9f3ddd4253dc622fbe5d4937447c
maksyuki <maksyuki@126.com>
2026-03-12 12:02:03 +0800

    fix: limit the fixed version for ihp pdk

--- pdk/setup.py
+++ pdk/setup.py
@@ -5,7 +5,6 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-os.system(f'git clone --recursive --depth 1 https://github.com/IHP-GmbH/IHP-Open-PDK.git {SCRIPT_DIR}/IHP-Open-PDK')
+os.system(f'git clone --depth 1 https://github.com/IHP-GmbH/IHP-Open-PDK.git {SCRIPT_DIR}/IHP-Open-PDK')
+os.system(f'cd {SCRIPT_DIR}/IHP-Open-PDK && git checkout 6ecd4ad0bb27c5150cf17e474c8f595c42fa44e2 && git submodule init && cd ..')
 os.system(f'git clone --recursive --depth 1 https://github.com/openecos-projects/icsprout55-pdk.git {SCRIPT_DIR}/icsprout55-pdk')
-# os.system(f'git clone --recursive --depth 1 https://github.com/google/skywater-pdk.git {SCRIPT_DIR}/skywater-pdk')
-# os.system(f'git clone --recursive --depth 1 https://github.com/google/gf180mcu-pdk.git {SCRIPT_DIR}/gf180mcu-pdk')
